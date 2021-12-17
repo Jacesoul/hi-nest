f6dd2a1f66981c585306ab4708b1126c18993fd2
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-12-17 17:57:37 +0900

    Testing update

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -13,6 +13,10 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  afterAll(() => {
+    // 데이터베이스를 깨끗하게 정리해주는 function을 넣을수 있다.
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
@@ -57,7 +61,7 @@ describe('MoviesService', () => {
       const afterDelete = service.getAll().length;
       expect(afterDelete).toBeLessThan(beforeDelete);
     });
-    it('should return a 404', () => {
+    it('should throw a NotFoundException', () => {
       try {
         service.deleteOne(999);
       } catch (e) {
@@ -77,6 +81,26 @@ describe('MoviesService', () => {
       const afterCreate = service.getAll().length;
       console.log(beforeCreate, afterCreate);
       expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.createMovie({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: 'Updated Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+    it('should throw a NotFoundException', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
     });
   });
 });
